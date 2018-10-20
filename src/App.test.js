2681ae91e0a22da522bfbb56f49c2da8921e1da1
Meghna Srivastava <meghna.s1906@gmail.com>
2018-10-20 19:26:25 +0200

    made simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,23 @@ import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-test('renders without crashing', () => {
-  const wrapper = shallow(<App />)
+test('renders without error', () => {
+
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+
+test('clicking button increments counter display', () => {
+
 });
