70d06e23e24e94445fc2a843e4cdea6bd0f6256c
Meghna Srivastava <meghna.s1906@gmail.com>
2018-10-21 17:50:36 +0200

    used setState method by shallow wrapper to track the next state

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16.3';
 import App from './App';
+import { wrap } from 'module';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
@@ -9,12 +10,14 @@ Enzyme.configure({adapter: new EnzymeAdapter()});
 * Factory function to create a ShallowWrapper for the App component
 * @function setup
 * @param {object} props - Component props specific to this setup
-* @param {any} state - Initial state for setup
+* @param {object} state - Initial state for setup
 * @returns {ShallowWrapper}
 */
 
 const setup = (props={}, state=null) => {
-  return shallow(<App {...props} />);
+  const wrapper = shallow(<App {...props} />);
+  if(state) wrapper.setState(state);
+  return wrapper;
 }
 
 /**
